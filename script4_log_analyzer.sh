c940d336c614667ac1a8db5a1547e986b6ff9893
Aaryan <aaryanmonster919@gmail.com>
2026-03-31 21:25:27 +0530

    Update author information in log analyzer script

--- script4_log_analyzer.sh
+++ script4_log_analyzer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ============================================================
 # Script 4: Log File Analyzer
-# Author: Aaryan Maurya | Roll: 24BAI10259
+# Author: Aaryan Aggarwal | Roll: 24Bcg10104
 # Course: Open Source Software | Software Choice: Git
 # Description: Reads a log file, counts keyword occurrences,
 #              and shows the last 5 matching lines
