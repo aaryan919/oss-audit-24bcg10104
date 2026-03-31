0df57652959536cca4821fc5c313601e0729a6d2
Aaryan <aaryanmonster919@gmail.com>
2026-03-31 21:24:22 +0530

    Update script3_disk_permission_auditor.sh

--- script3_disk_permission_auditor.sh
+++ script3_disk_permission_auditor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # ============================================================
 # Script 3: Disk and Permission Auditor
-# Author: Aaryan Maurya | Roll: 24BAI10259
+# Author: Aaryan Aggarwal | Roll: 24Bcg10104
 # Course: Open Source Software | Software Choice: Git
 # Description: Loops through key system directories and
 #              reports their size, owner, and permissions
